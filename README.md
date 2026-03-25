a4b6f3a8667b31c62dc51a7aea20e43c70c81c9d
204g1a05a7 <204g1a05a7@srit.ac.in>
2026-03-25 19:29:31 +0530

    Initial commit: Organized IPL Dashboard project structure

---
+++ README.md
@@ -0,0 +1,57 @@
+# Indian-Premier-league
+Data analysis of IPL of Last 3 seasons
+## TABLE OF CONTENTS
+- [Project overview](#project-overview)
+- [Live Dashboard](#live-dashboard)
+- [Data Sources](#data-sources)
+- [Methodology](#methodology)
+- [Key Insights](#key-insights)
+## Project Overview
+### Introduction:
+The Indian Premier League (IPL) has emerged as one of the most popular cricket tournaments globally, showcasing top-notch cricketing talent and attracting a massive audience. This project aims to analyze the data from the last three seasons of IPL using SQL, Power BI, and Excel to derive insights into player performance, team dynamics, and match outcomes.
+
+### Objectives:
+1.To analyze player performance metrics such as batting averages, bowling economy rates.
+
+2.To evaluate team performance based on win-loss ratios, run rates,boundaries.
+
+3.To identify trends and patterns in match outcomes, including factors influencing victories and defeats.
+
+4.To visualize key findings through interactive dashboards for easy interpretation and decision-making.
+### live dashboard
+[Live link](https://app.powerbi.com/view?r=eyJrIjoiNjU1MjYzYWQtNDgzMS00YTA4LWJmMzMtYTNkYjgzMzdjNzAwIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9)
+### Data Sources:
+https://codebasics.io/challenge/codebasics-resume-project-challenge (challenge10)
+![Screenshot 2024-04-19 210652](https://github.com/Github-sanket07sett/AFC-Asian-Cup-2023-QATAR-/assets/137095374/092ed782-d244-441d-b253-12fa51cdcfc8)
+
+
+### Methodology:
+
+#### Data Collection: Extract relevant data sets covering the last three IPL seasons, including match details, player statistics, and team performances.
+
+#### Data Cleaning: Preprocess the data to handle missing values, inconsistencies, and outliers to ensure accuracy and reliability.
+
+#### Data Analysis with SQL: Utilize SQL queries to perform in-depth analysis of structured data, including aggregations, joins, and filtering to derive meaningful insights.
+
+#### Visualization with Power BI: Create interactive dashboards and visualizations using Power BI to present key metrics, trends, and patterns discovered during the analysis phase.
+
+### Key insights:
+#### TEAM INSIGHTS:
+1. CSK scored highest runs (7372).
+2. GT has best batting average with 32.28.
+3. CSK with highest batting strike rate 139.54.
+4. RR has highest boundary% (64.53%).
+#### BATTING INSIGHTS
+1. Gill scored 1851 in last 3 seasons.(2023 orange cup winner).
+2. Klassen has best strike rate (177.08).
+3. KL Rahul has best batting average(51).
+4. Green has highest Boundary%(65%).
+5. Faf hits 169 sixes in last 3 seasons.
+6. Gill hits 186 fours in last 3 seasons.
+#### BOWLING INSIGHTS
+1. Shami takes 67 wickets in last 3 seasons
+2. Sunil Narine has best economy 6.6
+3. Shami bowled 47.65% dot ball.
+4. Krunal pandya bowled with 23.42 bowling average.
+5. Chahal conceded most boundaries(82 4s+59 6s).
+    
---
+++ sql/ipl_schema.sql
@@ -0,0 +1,162 @@
+show databases;
+use ipl;
+
+ #won_by biggest margin_by each team 
+select max(margin), `year`, `won_by`
+ FROM `ipl`.`match_summary` where `won_by`='runs' and winner='RCB' group by `year`;
+
+select * from batting_summary;
+
+# most runs in last 3 ipl
+SELECT batsmanName,sum(runs) as total_runs FROM batting_summary group by batsmanName order by total_runs desc limit 10;
+
+#highest run scorer in each year
+SELECT batsmanName,sum(runs) as total_runs FROM batting_summary inner join `ipl`.`match_summary` 
+on batting_summary.match_id=`ipl`.`match_summary`.match_id
+where `year`= 2022 group by batsmanName order by total_runs desc limit 10;
+ 
+#most wickets by bowlingstyle
+select bowlingStyle,sum(wickets) as Total_wickets  from bowling_summary inner join players
+on bowling_summary.bowlerName= `players`.`name`
+where bowlingStyle in('Right arm Fast','Right arm Fast Medium','Right arm Medium fast')
+group by bowlingStyle order by Total_wickets ; 
+
+
+#most runs By Playing Role
+SELECT playingRole,sum(runs) as total_runs FROM batting_summary 
+inner join players
+on batting_summary.batsmanName =  `players`.`name`
+group by playingRole order by total_runs desc ;
+
+#total dismissed by each batsman
+SELECT batsmanName, 
+       COUNT(CASE WHEN `out/not_out`  = 'out' THEN 1 END) AS total_dismissed
+FROM batting_summary
+GROUP BY batsmanName;
+
+#run getter by batting style
+SELECT battingStyle,sum(`wickets`) as total_wickets FROM `ipl`.`bowling_summary` 
+inner join players
+on bowling_summary.bowlerName=players.name
+group by battingStyle 
+order by total_wickets desc limit 10;
+
+
+# stike rate top 10
+select batsmanName,(sum(runs)/sum(balls))*100 as Strike_rate,playingRole  from batting_summary
+inner join players 
+on batting_summary.batsmanName=players.`name` 
+where playingRole in('Top Order batter','Middle order Batter','All rounder','Wicketkeeper','Bowler')
+group by batsmanName,playingRole
+having sum(balls)>=60
+order by Strike_rate desc limit 10;
+
+#strike rate by PlayingRole
+SELECT playingRole,(sum(runs)/sum(balls))*100 as Strike_rate FROM batting_summary 
+inner join players
+on batting_summary.batsmanName =  `players`.`name`
+group by playingRole order by Strike_rate desc ;
+
+
+
+#top 10 batsman  w.r.t batting average
+SELECT batsmanName,(sum(runs)/ COUNT(CASE WHEN `out/not_out`  = 'out' THEN 1 END)) as batting_average,playingRole
+FROM batting_summary
+inner join players 
+on batting_summary.batsmanName=players.`name` 
+GROUP BY batsmanName,playingRole
+having sum(balls)>60
+order by batting_average desc limit 10;
+
+#boundary percentage
+SELECT distinct(batsmanName),((sum(`4s`)*4+sum(`6s`)*6)/sum(runs))*100 as total_boundary_percentage,playingRole 
+from `ipl`.`batting_summary`
+inner join players
+on batting_summary.batsmanName=players.`name` 
+where playingRole in ('Top Order batter','Middle order Batter','All rounder','Wicketkeeper','Bowler')
+group by batsmanName,playingRole 
+having sum(balls)>60
+order by total_boundary_percentage desc limit 5;
+
+#most boundaries by player
+select batsmanName,sum(`4s`) as total_fours,sum(`6s`) as total_sixes from  `ipl`.`batting_summary` group by batsmanName order by total_sixes desc;
+
+
+# top 10 wicket taker
+SELECT bowlerName,sum(`wickets`) as total_wickets FROM `ipl`.`bowling_summary` group by bowlerName order by total_wickets desc limit 10;
+
+# most wickets in each year
+SELECT bowlerName,sum(wickets) as total_wickets FROM bowling_summary inner join `ipl`.`match_summary` 
+on bowling_summary.match_id=`ipl`.`match_summary`.match_id
+where `year`= 2022 group by bowlerName order by total_wickets desc limit 10;
+
+# top 10 bowlers with most dot ball percentage
+SELECT bowlerName,
+      round( (SUM(`0s`) / (SUM(overs) * 6)) * 100,2) AS Dot_Percentage,playingRole
+FROM bowling_summary
+inner join players
+on bowling_summary.bowlerName=players.`name` 
+where playingRole in ('Bowler','All rounder')
+GROUP BY bowlerName ,playingRole
+order by Dot_percentage desc limit 10;
+
+#most maidens
+select bowlerName,sum(maiden) as total_maidens from bowling_summary group by bowlerName order by total_maidens desc limit 5;
+
+#economy rate
+select bowlerName,round((sum(runs)/(sum(overs))),2) as economy_rate from bowling_summary 
+inner join players
+on bowling_summary.bowlerName=players.`name` 
+where playingRole in ('Bowler','All rounder')
+group by bowlerName 
+having sum(overs)*6>=60
+order by economy_rate;
+
+#bowling Average
+select bowlerName,(sum(runs)/sum(wickets)) as Bowling_avg,sum(wickets) AS TOTAL_WICKETS from bowling_summary 
+inner join players
+on bowling_summary.bowlerName=players.`name` 
+where playingRole in ('Bowler','All rounder')
+group by bowlerName 
+having sum(overs)*6>=60
+order by sum(wickets) desc
+;
+
+#bowling average by each year for particular bowler
+select bowlerName,round((sum(runs)/sum(wickets)),2) as Bowling_avg,sum(wickets) AS TOTAL_WICKETS from bowling_summary 
+inner join `ipl`.`match_summary` 
+on bowling_summary.match_id=`ipl`.`match_summary`.match_id
+where  `year` =2022
+group by  bowlerName 
+having sum(overs)*6>=60
+order by  Bowling_avg asc limit 10;
+
+
+#total wins in last 3 years
+SELECT winner,count(`winner`) as total_wins FROM `ipl`.`match_summary` group by winner order by total_wins desc;
+
+# Chasing Time winner
+SELECT `team2`,COUNT(*) as Won_by_chase
+FROM `ipl`.`match_summary`
+where team2=winner
+group by team2
+order by Won_by_chase desc;
+
+#target defend
+SELECT `team1`,COUNT(*) as target_defend
+FROM `ipl`.`match_summary`
+where team1=winner
+group by team1
+order by target_defend desc;
+
+#won bY
+SELECT `won_by`,count(won_by) AS won_by_count FROM `ipl`.`match_summary` where  year =2021 group by won_by;
+
+# Batting avg by Playing Role
+SELECT (sum(runs)/ COUNT(CASE WHEN `out/not_out`  = 'out' THEN 1 END)) as batting_average,playingRole
+FROM batting_summary
+inner join players 
+on batting_summary.batsmanName=players.`name` 
+GROUP BY playingRole
+having sum(balls)>60
+order by batting_average desc limit 10;
